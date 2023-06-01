2fd727d551c124d49fb8c786bb27a7314129feff
Gawaisuraj12 <svsgawai@gmail.com>
2023-06-01 15:21:36 +0530

    Added

---
+++ switchdish.js
@@ -0,0 +1,18 @@
+let dish=3;
+
+switch(dish)
+{
+    case 1 :
+        console.log("You Ordered Dal Tadka of Rs. 120/- ");
+        break; 
+
+    case 2 :
+        console.log("You Ordered Shevbhaji of Rs. 110/- ");
+        break; 
+    case 3 :
+            console.log("You Ordered Paneer Tikka of Rs. 160/- ");
+            break; 
+    
+default : 
+        console.log("Sorry You are not Ordered anything ");        
+}
